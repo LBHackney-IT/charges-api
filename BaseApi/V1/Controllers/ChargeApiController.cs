f30dff416310891630b898a7a77268d485317c7e
Hamid Jolany <hamid.jolany@nudgedigital.co.uk>
2021-07-06 12:56:40 +0430

    Code review correction

--- BaseApi/V1/Controllers/ChargeApiController.cs
+++ BaseApi/V1/Controllers/ChargeApiController.cs
@@ -4,8 +4,10 @@ using ChargeApi.V1.UseCase.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using ChargeApi.V1.Factories;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ChargeApi.V1.Controllers
 {
@@ -37,11 +39,7 @@ namespace ChargeApi.V1.Controllers
             _removeUseCase = removeUseCase;
             _updateUseCase = updateUseCase;
         }
-        /// <summary>
-        /// Corresponding charge data according the id 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+
         [ProducesResponseType(typeof(ChargeResponseObject), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -50,13 +48,17 @@ namespace ChargeApi.V1.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Get(Guid id)
         {
-            if (id == null)
-                return BadRequest();
-
-            var charge = await _getByIdUseCase.ExecuteAsync(id).ConfigureAwait(false);
-            if (charge == null)
-                return NotFound(id);
-            return Ok(charge);
+            try
+            {
+                var charge = await _getByIdUseCase.ExecuteAsync(id).ConfigureAwait(false);
+                if (charge == null)
+                    return NotFound(id);
+                return Ok(charge);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [ProducesResponseType(typeof(ChargeResponseObjectList), StatusCodes.Status200OK)]
@@ -66,50 +68,87 @@ namespace ChargeApi.V1.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllAsync([FromQuery]string type,[FromQuery] Guid targetid)
         {
-            var charges = await _getAllUseCase.ExecuteAsync(type,targetid).ConfigureAwait(false);
-            if (charges == null)
-                return NotFound();
-            return Ok(charges);
+            try
+            {
+                var charges = await _getAllUseCase.ExecuteAsync(type, targetid).ConfigureAwait(false);
+                if (charges == null)
+                    return NotFound(targetid);
+                return Ok(charges);
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        [ProducesResponseType(StatusCodes.Status409Conflict)]
-        [Route("")]
+        [ProducesResponseType(typeof(ChargeResponseObject), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
         public async Task<IActionResult> Post(Charge charge)
         {
-            var _charge = await _getByIdUseCase.ExecuteAsync(charge.Id).ConfigureAwait(false);
-            if (_charge != null)
-                return Conflict("This record is exists");
-            await _addUseCase.ExecuteAsync(charge).ConfigureAwait(false);
-            return RedirectToAction("Get", new { id = charge.Id });
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _addUseCase.ExecuteAsync(charge).ConfigureAwait(false);
+
+                    return RedirectToAction("Get", new {id = charge.Id});
+                }
+                else
+                {
+                    return BadRequest(ModelState.Values.First().Errors[0].ErrorMessage);
+                }
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
+        [ProducesResponseType(typeof(ChargeResponseObject),StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        /// [ProducesResponseType(StatusCodes.Status400BadRequest)] ???
         [Route("{id}")]
         [HttpPut]
         public async Task<IActionResult> Put([FromRoute] Guid id, [FromBody] Charge charge)
         {
-            if (id != charge.Id)
-                return NotFound(id); // BadRequest ???
+            try
+            {
+                if (id != charge.Id)
+                    return BadRequest(id);
 
-            await _updateUseCase.ExecuteAsync(charge).ConfigureAwait(false);
-            return RedirectToAction("Get", new { id = charge.Id });
+                ChargeResponseObject chargeResponseObject = await _getByIdUseCase.ExecuteAsync(id).ConfigureAwait(false);
+                if (chargeResponseObject == null)
+                    return NotFound(id);
+
+                await _updateUseCase.ExecuteAsync(charge).ConfigureAwait(false);
+                return RedirectToAction("Get", new { id = charge.Id });
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [Route("{id}")]
         [HttpDelete]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
-
-            ChargeResponseObject charge = await _getByIdUseCase.ExecuteAsync(id).ConfigureAwait(false);
-            if (charge == null)
-                return NotFound(id);
-
-            await _removeUseCase.ExecuteAsync(id).ConfigureAwait(false);
-            return NoContent();
+            try
+            {
+                ChargeResponseObject charge = await _getByIdUseCase.ExecuteAsync(id).ConfigureAwait(false);
+                if (charge == null)
+                    return NotFound(id);
+                await _removeUseCase.ExecuteAsync(id).ConfigureAwait(false);
+                return NoContent();
+            }
+            catch (FormatException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
--- BaseApi/V1/Domain/detailedCharges.cs
+++ BaseApi/V1/Domain/detailedCharges.cs
@@ -8,26 +8,11 @@ namespace ChargeApi.V1.Domain
 {
     public class DetailedCharges
     {
-        private decimal _amount;
-
         public string Type { get; set; }
         public string SubType { get; set; }
         public string Frequency { get; set; }
-        public decimal Amount
-        {
-            get
-            {
-                return _amount;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentOutOfRangeException("Amount", "Please enter the valid value");
-                }
-                _amount = value;
-            }
-        }
+        [Range(0,double.MaxValue,ErrorMessage = "The amount value is wrong")]
+        public decimal Amount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
