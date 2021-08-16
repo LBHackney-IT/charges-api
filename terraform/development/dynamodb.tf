62a5040ea93ccfb67f91d2b1643ab2177b03c338
Sandip <Sandip@DESKTOP-8GSTDPG>
2021-08-16 19:23:06 +0530

    Added Terraform files

---
+++ terraform/development/dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "chargesapi_dynamodb_table" {
+    name                  = "Accounts"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "id"
+
+    attribute {
+        name              = "id"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "charges-api-${var.environment_name}"
+        Environment       = var.environment_name
+        terraform-managed = true
+        project_name      = var.project_name
+    }
+
+    point_in_time_recovery {
+        enabled           = true
+    }
+}
